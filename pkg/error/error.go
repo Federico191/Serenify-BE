adcc29112608e8f5ab196d02cbdfa2d099136e8b
Federico191 <federicoberto708@gmail.com>
2024-05-10 02:56:09 +0700

    feat : add new errors

--- pkg/error/error.go
+++ pkg/error/error.go
@@ -3,6 +3,8 @@ package error
 import "errors"
 
 var ErrEmailAlreadyExists = errors.New("email already exists")
+var ErrRecordAlreadyExists = errors.New("record already exists")
 var ErrRecordNotFound = errors.New("record not found")
 var ErrInvalidEmailPassword = errors.New("invalid email/password")
 var ErrEmailNotVerified = errors.New("email not verified")
+var ErrNotAuthorize = errors.New("not authorize to perform this action")
