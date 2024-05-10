496d5507c4c659865bc7af07e85468ad15b9bc0b
Federico191 <federicoberto708@gmail.com>
2024-05-10 22:22:31 +0700

    refactor : add email reset password

--- pkg/email/email.go
+++ pkg/email/email.go
@@ -12,7 +12,8 @@ import (
 const appName = "Serenify"
 
 type EmailItf interface {
-	SendEmail(user *entity.User, verificationCode string) error
+	SendEmailVerification(user *entity.User, verificationCode string) error
+	SendEmailResetPassword(user *entity.User, token string) error
 }
 
 type Email struct {
@@ -22,8 +23,8 @@ func NewEmail() EmailItf {
 	return &Email{}
 }
 
-func (e *Email) SendEmail(user *entity.User, verificationCode string) error {
-	url := "https://bb9d-180-248-26-61.ngrok-free.app/api/v1/auth/verify-email/" + verificationCode
+func (e *Email) SendEmailVerification(user *entity.User, verificationCode string) error {
+	url := "https://"+ os.Getenv("APP_URL") +"/api/v1/auth/verify-email/" + verificationCode
 
 	textString := fmt.Sprintf(`
 		<html>
@@ -73,25 +74,79 @@ func (e *Email) SendEmail(user *entity.User, verificationCode string) error {
     </html>
 `,
 		appName, user.FullName, appName, url, appName, appName)
-	
-		mailer := gomail.NewMessage()
-		mailer.SetHeader("From", fmt.Sprintf("<%s>", os.Getenv("SMTP_USER")))
-		mailer.SetHeader("To", fmt.Sprintf("<%s>", user.Email))
-		mailer.SetHeader("Subject", "Verify Your Email")
-		mailer.SetBody("text/html", textString)
 
-		port, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
-		if err != nil {
-			return err
-		}
+	return sendEmail(user.Email, "Verify Email", textString)
+}
 
-		dialer := gomail.NewDialer(os.Getenv("SMTP_HOST"), port, 
-		os.Getenv("SMTP_USER"), os.Getenv("SMTP_PASSWORD"))
+func (e *Email) SendEmailResetPassword(user *entity.User, token string) error {
+	url := "https://"+ os.Getenv("APP_URL") +"/v1/api/auth/reset-password/" + token
+	textString := fmt.Sprintf(`
+        <html>
+        <head>
+            <style>
+            body {
+                font-family: Arial, sans-serif;
+            }
+            .container {
+                max-width: 600px;
+                margin: 0 auto;
+            }
+            .header {
+                background-color: #f2f2f2;
+                padding: 20px;
+                text-align: center;
+            }
+            .content {
+                padding: 20px;
+            }
+            .button {
+                display: inline-block;
+                background-color: #007bff;
+                color: #fff;
+                padding: 10px 20px;
+                text-decoration: none;
+                border-radius: 5px;
+            }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="header">
+                    <h2>Reset Your Password</h2>
+                </div>
+                <div class="content">
+                    <p>Dear %s,</p>
+                    <p>You recently requested to reset your password for your %s account. Please click the button below to reset your password:</p>
+                    <a href="%s" class="button">Reset Password</a>
+                    <p>If you did not request a password reset, you can safely ignore this email.</p>
+                    <p>Thank you.</p>
+                    <p>Regards,<br/>The %s Team</p>
+                </div>
+            </div>
+        </body>
+        </html>
+        `, user.FullName, appName, url, appName)
 
-		err = dialer.DialAndSend(mailer)
-		if err != nil {
-			return err
-		}
+	return sendEmail(user.Email, "Reset Your Password", textString)
+}
 
-		return nil
+func sendEmail(to, subject, body string) error {
+	mailer := gomail.NewMessage()
+	mailer.SetHeader("From", fmt.Sprintf("<%s>", os.Getenv("SMTP_USER")))
+	mailer.SetHeader("To", fmt.Sprintf("<%s>", to))
+	mailer.SetHeader("Subject", subject)
+	mailer.SetBody("text/html", body)
+
+	port, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
+	if err != nil {
+		return err
+	}
+
+	dialer := gomail.NewDialer(os.Getenv("SMTP_HOST"), port, os.Getenv("SMTP_USER"), os.Getenv("SMTP_PASSWORD"))
+	err = dialer.DialAndSend(mailer)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
